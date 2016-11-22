4e0a1cda5b5d7559749effdabd92dc27d3f1c235
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-11-21 21:15:46 -0800

    transfer file

--- Sources/OblFunc.swift
+++ Sources/OblFunc.swift
@@ -1,64 +1,15 @@
 //Implements function in Oblivion Language
 
-
-//Implementation of environment object
-
-class Env {
-    var defs:[String:OblVal]
-    var parent:Env?
-    
-    //checks if current env is global
-    var isRoot: Bool {
-        return self.parent == nil
+class OblFunc {
+    //inner enum to represent arguments
+    enum operand {
+        case value(OblVal)
+        case param(String)
+        case fn(OblFunc)
     }
     
-    //global env initializer
-    init(){
-        self.defs = [String:OblVal]()
-    }
-    
-    //local env initializer
-    init(args:Env) {
-        self.parent = args
-        self.defs = [String:OblVal]()
-    }
-    
-    //returns a new Env that's linked to the env that calls this method
-    func clone() -> Env {
-        return Env(args:self)
-    }
-    
-    //getter method that checks local env, then goes up in parent branch
-    func get(index:String) -> OblVal {
-        if let val = self.defs[index] {
-            return val
-        }
-        else {
-            if self.parent != nil {
-                return self.parent!.get(index:index)
-            }
-            else {
-                return OblVal.undef
-            }
-        }
-    }
-    //basic setter method
-    func set(index:String, val:OblVal) -> Void {
-        self.defs[index] = val
-    }
-    //checks if a key is present
-    func contains(index:String) -> Bool {
-        return self.defs[index] != nil
-    }
-    
-    //updats the environment based on incoming dictionaries of OblVals
-    func update(dict:[String:OblVal]) -> Void {
-        for (key, value) in dict {
-            self.defs[key] = value
-        }
-    }
-    //initalizes an empty variable assigned to null.
-    func InitNull(index:String) -> Void {
-        self.defs[index] = OblVal.null
-    }
+    var params:[String]?
+    var argumnts:[operand]?
+    var env:Env?
 }
+
